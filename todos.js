836bf91259ace9a01521fa83bd24dc68108fb8d5
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2021-12-16 12:31:27 +1100

    add layout and seed-data

---
+++ todos.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+const host = "localhost";
+const port = 3003;
+
+// Static data for initial testing
+let todoLists = require("./lib/seed-data");
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+// Middleware to use
+app.use(morgan("common"));
+app.use(express.static("public"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("lists");
+});
+
+// Listener
+app.listen(port, host, () => {
+  console.log(`Listening on port ${port} of ${host}...`);
+});
