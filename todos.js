c941bdf3b954b39ba5d6ef7e27441523dc86bda3
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2021-12-19 16:57:10 +1100

    add new todo view and routes

--- todos.js
+++ todos.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const TodoList = require("./lib/todolist");
+
 const app = express();
 const host = "localhost";
 const port = 3003;
@@ -14,10 +16,50 @@ app.set("view engine", "pug");
 // Middleware to use
 app.use(morgan("common"));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+const compareByTitle = (todoA, todoB) => {
+  let titleA = todoA.title.toLowerCase();
+  let titleB = todoB.title.toLowerCase();
+
+  if (titleA < titleB) {
+    return -1;
+  } else if (titleA > titleB) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+// return the list of todo lists sorted by completion status and title
+const sortTodoLists = lists => {
+  const undone = lists.filter(todoList => !todoList.isDone());
+  const done = lists.filter(todoList => todoList.isDone());
+
+  undone.sort(compareByTitle);
+  done.sort(compareByTitle);
+
+  return [...undone, ...done];
+};
 
 // Routes
 app.get("/", (req, res) => {
-  res.render("lists", { todoLists });
+  res.redirect("/lists");
+});
+
+app.get("/lists", (req, res) => {
+  res.render("lists", { todoLists: sortTodoLists(todoLists) });
+});
+
+app.get("/lists/new", (req, res) => {
+  res.render("new-list");
+});
+
+// Create a new todo list
+app.post("/lists", (req, res) => {
+  const title = req.body.todoListTitle.trim();
+  todoLists.push(new TodoList(title));
+  res.redirect("/lists");
 });
 
 // Listener
